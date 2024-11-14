d2495da541d4b0c5fbb5d6f3f5bf2d3aadaf5c43
Josh Purtell <jmvpurtell@gmail.com>
2024-11-13 21:40:53 -0800

    save progress

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="synth-sdk",
-    version="0.2.64",
+    version="0.2.65",
     packages=find_packages(),
     install_requires=[
         "opentelemetry-api",
