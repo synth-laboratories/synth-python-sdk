5510a83e0a181fdf347de91bb17766717bccf074
Josh Purtell <jmvpurtell@gmail.com>
2024-11-17 17:32:46 -0800

    move build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-# Build the distribution files
-python -m build
-
-# Upload to TestPyPI (optional but recommended)
-python -m twine upload --repository testpypi dist/*
-
-# Upload to PyPI
-python -m twine upload dist/*
