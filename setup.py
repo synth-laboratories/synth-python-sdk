c3096269556bee978d46864dc3862f85f8234780
doku88 <ctmk2020@gmail.com>
2024-12-15 20:20:25 -0800

    synth-sdk 0.2.94 updated to PyPi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="synth-sdk",
-    version="0.2.85",
+    version="0.2.94",
     packages=find_packages(),
     install_requires=[
         "opentelemetry-api",
