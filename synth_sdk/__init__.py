8fd2f952c37743b6d6a2dc2df1f4b6ab1325c390
Josh Purtell <jmvpurtell@gmail.com>
2025-02-06 11:24:34 -0800

    pull metadata into dev

--- synth_sdk/__init__.py
+++ synth_sdk/__init__.py
@@ -9,3 +9,7 @@ __version__ = version("synth-sdk")
 from .tracing import *  # noqa
 
 # You can also add other package-level imports and initialization here
+from synth_sdk.tracing.abstractions import EventPartitionElement, SystemTrace, TrainingQuestion, RewardSignal
+from synth_sdk.tracing.decorators import trace_event_async,trace_event_sync
+from synth_sdk.provider_support.openai import OpenAI, AsyncOpenAI
+from synth_sdk.provider_support.anthropic import Anthropic, AsyncAnthropic
